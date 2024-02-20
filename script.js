a1e748a2bd493b0a18067586717b773b1d505001
TomBoutin <144112026+TomBoutin@users.noreply.github.com>
2024-02-20 11:12:54 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* Tableau de positions pour les déchets */
 let objPosition = [
   { x: -20.57778, y: 0.06, z: -53.53054 },
   { x: 42.27, y: 0.06, z: -40.98 },
@@ -21,83 +22,7 @@ let objPosition = [
   { x: 46.112, y: 0.06, z: -17.887 },
 ];
 
-//   <!-- canettes -->
-//   <a-entity gltf-model="#canette" id="canette1" position="14.217 0.04 -29.249" scale="2 2 2"
-//     class="collectible clickable">
-//   </a-entity>
-//   <a-sound id="son-canette1" src="src: url(./Son/Minecraft Item Drop Sound Effect!.mp3)" volume=".6"
-//     position="14.217 0.04 -29.249">
-//   </a-sound>
-
-//   <a-entity gltf-model="#canette" id="canette2" position="67.713 0.04 -66.943" scale="2 2 2"
-//     class="collectible clickable">
-//   </a-entity>
-//   <a-sound id="son-canette2" src="src: url(./Son/Minecraft Item Drop Sound Effect!.mp3)" volume=".6"
-//     position="67.713 0.03957 -66.943">
-//   </a-sound>
-
-//   <a-entity gltf-model="#canette" id="canette3" position="35.99 0.343 -0.632" scale="2 2 2"
-//     class="collectible clickable">
-//   </a-entity>
-//   <a-sound id="son-canette3" src="src: url(./Son/Minecraft Item Drop Sound Effect!.mp3)" volume=".6"
-//     position="35.99 0.343 -0.632">
-//   </a-sound>
-
-//   <!-- Bouteilles -->
-//   <a-entity gltf-model="#bouteille" id="bouteille1" scale="0.1 0.1 0.1" position="43.906 4.99 -5.345"
-//     class="collectible clickable">
-//   </a-entity>
-//   <a-sound id="son-bouteille1" src="src: url(./Son/Minecraft Item Drop Sound Effect!.mp3)" volume=".6"
-//     position="43.906 4.99 -5.345">
-//   </a-sound>
-
-//   <a-entity gltf-model="#bouteille" id="bouteille2" scale="0.1 0.1 0.1" position="31.406 1.643 -19.454"
-//     class="collectible clickable">
-//   </a-entity>
-//   <a-sound id="son-bouteille2" src="src: url(./Son/Minecraft Item Drop Sound Effect!.mp3)" volume=".6"
-//     position="31.406 1.643 -19.454">
-//   </a-sound>
-
-//   <a-entity gltf-model="#bouteille" id="bouteille3" scale="0.1 0.1 0.1" position="40.111 0.391 -51.4"
-//   class="collectible clickable">
-// </a-entity>
-// <a-sound id="son-bouteille3" src="src: url(./Son/Minecraft Item Drop Sound Effect!.mp3)" volume=".6"
-//   position="40.111 0.391 -51.4">
-// </a-sound>
-//   <!-- Boite de pizza -->
-//   <a-entity gltf-model="#boite_pizza" id="boite_pizza1" rotation="0 120 0" position="59.261 -0.008 24.634"
-//     class="collectible clickable">
-//   </a-entity>
-//   <a-sound id="son-boite_pizza1" src="src: url(./Son/Minecraft Item Drop Sound Effect!.mp3)" volume=".6"
-//     position="59.261 -0.00836 24.634">
-//   </a-sound>
-
-//   <!-- Sac poubelle noir -->
-//   <a-entity gltf-model="#sac-poubelle_noir" id="sac-poubelle_noir1"
-//     rotation="20.060565647842474 38.03694914534996 1.040491355957575" position="13.081 0.50342 -56.462"
-//     scale="3 3 3" class="collectible clickable">
-//   </a-entity>
-//   <a-sound id="son-sac-poubelle_noir1" src="src: url(./Son/Minecraft Item Drop Sound Effect!.mp3)" volume=".6"
-//     position="13.081 0.50342 -56.462">
-//   </a-sound>
-
-//   <a-entity gltf-model="#sac-poubelle_noir" id="sac-poubelle_noir2"
-//     rotation="20.07 38.037 1.04" position="53.67 0.503 -75.1"
-//     scale="3 3 3" class="collectible clickable">
-//   </a-entity>
-//   <a-sound id="son-sac-poubelle_noir2" src="src: url(./Son/Minecraft Item Drop Sound Effect!.mp3)" volume=".6"
-//     position="53.67 0.50342 -75.1">
-//   </a-sound>
-
-//   <!-- Sac poubelle gris -->
-//   <a-entity gltf-model="#sac-poubelle_gris" id="sac-poubelle_gris1"
-//     rotation="17.177 3.525 11.784" position="46.112 0.608 -17.887"
-//     scale="3 3 3" class="collectible clickable">
-//   </a-entity>
-//   <a-sound id="son-sac-poubelle_gris1" src="src: url(./Son/Minecraft Item Drop Sound Effect!.mp3)" volume=".6"
-//     position="46.112 0.60845 -17.887">
-//   </a-sound>
-
+/* Tableau de déchets */
 let dechets = [
   {
     id: "canette1",
@@ -185,20 +110,23 @@ function updateScore() {
     element.setAttribute(
       "text",
       "value: DÉCHETS:" +
-        count +
-        "/" +
-        nombreDechetsTotal +
-        "; color: #000000; width: 10; height:10; align: center; wrapCount: 20; font: ./Texture/font/Kanit-Bold-msdf.json; negate:false;; alphaTest: 10"
+      count +
+      "/" +
+      nombreDechetsTotal +
+      "; color: #000000; width: 10; height:10; align: center; wrapCount: 20; font: ./Texture/font/Kanit-Bold-msdf.json; negate:false;; alphaTest: 10"
     );
   });
 }
 
-// Fonction pour générer des déchets avec des positions aléatoires
+/* Fonction pour générer des déchets avec des positions aléatoires */
+
 dechets.forEach(function (dechet) {
+  // Choisit une position aléatoire dans le tableau objPosition
   let index = Math.floor(Math.random() * objPosition.length);
   let position = objPosition[index];
   objPosition.splice(index, 1); // Supprime la position utilisée du tableau
 
+  // Crée un élément a-entity pour chaque déchet avec les attributs nécessaires
   let entity = document.createElement("a-entity");
   let sound = document.createElement("a-sound");
   entity.setAttribute("gltf-model", dechet.model);
@@ -219,176 +147,35 @@ dechets.forEach(function (dechet) {
   sound.setAttribute("id", dechet.sound);
 
   document.querySelector("a-scene").appendChild(sound);
-  
+
+  // Ajoute un écouteur d'événement pour chaque déchet sur le clic
   entity.addEventListener("click", function () {
-    
+
     sound.components.sound.playSound();
+    updateScore();
+    var objet = entity;
+    objet.setAttribute("visible", "false");
+    objet.setAttribute("position", { y: -2 });
 
+  });
 
-  updateScore();
+  // Ajoute un écouteur d'événement pour chaque déchet avec les manettes VR
+  document.querySelector("#rightController").addEventListener("triggerdown", function () {
+    var intersectedElement = document.querySelector("[raycaster]").components.raycaster.intersectedEls[0];
 
-  var objet = entity;
-  objet.setAttribute("visible", "false");
-  objet.setAttribute("position", { y: -2 });
+    if (intersectedElement && intersectedElement.id === dechet.id) {
+      sound.components.sound.playSound();
+      updateScore();
+      var objet = entity;
+      objet.setAttribute("visible", "false");
+      objet.setAttribute("position", { y: -2 });
+    }
+  });
 
-})
-document.querySelector("a-scene").appendChild(entity);
+  document.querySelector("a-scene").appendChild(entity);
 
 });
 
-// generateDechets();
-
-
-// // Canettes
-// document.querySelector("#canette1").addEventListener("click", function () {
-//   // var score = document.querySelector('#score');
-//   // score.innerHTML = parseInt(score.innerHTML) + 5;
-//   var son = document.querySelector("#son-canette1");
-//   son.components.sound.playSound();
-
-//   updateScore();
-
-//   var objet = document.querySelector("#canette1");
-//   objet.setAttribute("visible", "false");
-//   objet.setAttribute("position", { y: -2 });
-// });
-
-// document.querySelector("#canette2").addEventListener("click", function () {
-//   // var score = document.querySelector('#score');
-//   // score.innerHTML = parseInt(score.innerHTML) + 5;
-//   var son = document.querySelector("#son-canette2");
-//   son.components.sound.playSound();
-
-//   var objet = document.querySelector("#canette2");
-
-//   updateScore();
-
-//   objet.setAttribute("visible", "false");
-//   objet.setAttribute("position", { y: -2 });
-// });
-
-// document.querySelector("#canette3").addEventListener("click", function () {
-//   // var score = document.querySelector('#score');
-//   // score.innerHTML = parseInt(score.innerHTML) + 5;
-//   var son = document.querySelector("#son-canette3");
-//   son.components.sound.playSound();
-
-//   var objet = document.querySelector("#canette3");
-
-//   updateScore();
-
-//   objet.setAttribute("visible", "false");
-//   objet.setAttribute("position", { y: -2 });
-// });
-
-// // Bouteilles
-
-// document.querySelector("#bouteille1").addEventListener("click", function () {
-//   // var score = document.querySelector('#score');
-//   // score.innerHTML = parseInt(score.innerHTML) + 5;
-//   var son = document.querySelector("#son-bouteille1");
-//   son.components.sound.playSound();
-
-//   var objet = document.querySelector("#bouteille1");
-
-//   updateScore();
-
-//   objet.setAttribute("visible", "false");
-//   objet.setAttribute("position", { y: -2 });
-// });
-
-// document.querySelector("#bouteille2").addEventListener("click", function () {
-//   // var score = document.querySelector('#score');
-//   // score.innerHTML = parseInt(score.innerHTML) + 5;
-//   var son = document.querySelector("#son-bouteille2");
-//   son.components.sound.playSound();
-
-//   var objet = document.querySelector("#bouteille2");
-
-//   updateScore();
-
-//   objet.setAttribute("visible", "false");
-//   objet.setAttribute("position", { y: -2 });
-// });
-
-// document.querySelector("#bouteille3").addEventListener("click", function () {
-//   // var score = document.querySelector('#score');
-//   // score.innerHTML = parseInt(score.innerHTML) + 5;
-//   var son = document.querySelector("#son-bouteille3");
-//   son.components.sound.playSound();
-
-//   var objet = document.querySelector("#bouteille3");
-
-//   updateScore();
-
-//   objet.setAttribute("visible", "false");
-//   objet.setAttribute("position", { y: -2 });
-// });
-
-// // Boite de pizza
-// document.querySelector("#boite_pizza1").addEventListener("click", function () {
-//   // var score = document.querySelector('#score');
-//   // score.innerHTML = parseInt(score.innerHTML) + 5;
-//   var son = document.querySelector("#son-boite_pizza1");
-//   son.components.sound.playSound();
-
-//   var objet = document.querySelector("#boite_pizza1");
-
-//   updateScore();
-
-//   objet.setAttribute("visible", "false");
-//   objet.setAttribute("position", { y: -2 });
-// });
-
-// // Sac poubelle noir
-// document
-//   .querySelector("#sac-poubelle_noir1")
-//   .addEventListener("click", function () {
-//     // var score = document.querySelector('#score');
-//     // score.innerHTML = parseInt(score.innerHTML) + 5;
-//     var son = document.querySelector("#son-sac-poubelle_noir1");
-//     son.components.sound.playSound();
-
-//     var objet = document.querySelector("#sac-poubelle_noir1");
-
-//     updateScore();
-
-//     objet.setAttribute("visible", "false");
-//     objet.setAttribute("position", { y: -2 });
-//   });
-
-// document
-//   .querySelector("#sac-poubelle_noir2")
-//   .addEventListener("click", function () {
-//     // var score = document.querySelector('#score');
-//     // score.innerHTML = parseInt(score.innerHTML) + 5;
-//     var son = document.querySelector("#son-sac-poubelle_noir2");
-//     son.components.sound.playSound();
-
-//     var objet = document.querySelector("#sac-poubelle_noir2");
-
-//     updateScore();
-
-//     objet.setAttribute("visible", "false");
-//     objet.setAttribute("position", { y: -2 });
-//   });
-
-// // Sac poubelle gris
-// document
-//   .querySelector("#sac-poubelle_gris1")
-//   .addEventListener("click", function () {
-//     // var score = document.querySelector('#score');
-//     // score.innerHTML = parseInt(score.innerHTML) + 5;
-//     var son = document.querySelector("#son-sac-poubelle_gris1");
-//     son.components.sound.playSound();
-
-//     var objet = document.querySelector("#sac-poubelle_gris1");
-
-//     updateScore();
-
-//     objet.setAttribute("visible", "false");
-//     objet.setAttribute("position", { y: -2 });
-//   });
 
 // /* ÉCOUTEURS D'ÉVÉNEMENTS POUR LES MANETTES VR */
 
